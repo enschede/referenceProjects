1a977a4043157f7d0070f1d714396193e8151f26
Marc Enschede <marc@marcenschede.nl>
2016-04-22 17:05:57 +0200

    Update matching json mappers

--- json-tests/src/test/java/app/mapping/RectangleMapperTest.java
+++ json-tests/src/test/java/app/mapping/RectangleMapperTest.java
@@ -1,6 +1,8 @@
 package app.mapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -19,15 +21,24 @@ public class RectangleMapperTest {
     }
 
     @Test
-    public void shouldReadAndWriteJsonWithMapper() throws IOException {
+    public void shouldWriteJsonWithMapper() throws IOException {
         Rectangle rectangle = new Rectangle(2, 2);
 
         ObjectMapper objectMapper1 = new ObjectMapper();
         objectMapper1.addMixIn(Rectangle.class, RectangleMapper.class);
 
-        System.out.println(objectMapper1.writeValueAsString(rectangle));
+        Assert.assertThat(objectMapper1.writeValueAsString(rectangle), CoreMatchers.is("{\"width\":2,\"height\":2}"));
+    }
+
+    @Test
+    public void shouldReadJsonWithMapper() throws IOException {
+
+        ObjectMapper objectMapper1 = new ObjectMapper();
+        objectMapper1.addMixIn(Rectangle.class, RectangleMapper.class);
 
         Rectangle rectangle1 = objectMapper1.readValue("{\"width\":2,\"height\":2}", Rectangle.class);
-        System.out.println(rectangle1.toString());
+
+        Assert.assertThat(rectangle1.getH(), CoreMatchers.is(2));
+        Assert.assertThat(rectangle1.getW(), CoreMatchers.is(2));
     }
 }
